290789aa2ac93e9d3c9cbce9bdfbf66592d75a57
DanielVanOnselen <danielvanons@gmail.com>
2026-04-18 11:55:00 +0200

    Delete WPR281 = PROJECT/WPR281 = PROJECT directory

--- WPR281 = PROJECT/WPR281 = PROJECT/people.js
+++
@@ -1,8 +0,0 @@
-// Member 1 scope note:
-// This page UI is complete
-// MEMBER 4: load and display people data from storage
-// We are gonna use static people values to add people (or if you disagree just say)
-document.addEventListener("DOMContentLoaded", () => {
-    // MEMBER 4:
-    // Retrieve people from storage and populate #peopleTableBody
-});
